6562b4704751be56d54aa0b1415a6dbd44457714
rizrmdhn <rizrmdhn.work@gmail.com>
2024-03-01 15:48:10 +0800

    add core module

---
+++ app/src/main/java/com/rizrmdhn/storyapp/MyApplication.kt
@@ -0,0 +1,29 @@
+package com.rizrmdhn.storyapp
+
+import android.app.Application
+import com.rizrmdhn.core.di.databaseModule
+import com.rizrmdhn.core.di.networkModule
+import com.rizrmdhn.core.di.preferencesModule
+import com.rizrmdhn.core.di.repositoryModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class MyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@MyApplication)
+            modules(
+                listOf(
+                    databaseModule,
+                    preferencesModule,
+                    networkModule,
+                    repositoryModule,
+                )
+            )
+        }
+    }
+}
--- core/src/main/java/com/rizrmdhn/core/di/CoreModule.kt
+++ core/src/main/java/com/rizrmdhn/core/di/CoreModule.kt
@@ -1,4 +1,85 @@
 package com.rizrmdhn.core.di
 
-class CoreModule {
+import androidx.room.Room
+import com.rizrmdhn.core.BuildConfig
+import com.rizrmdhn.core.data.StoryRepository
+import com.rizrmdhn.core.data.source.local.LocalDataSource
+import com.rizrmdhn.core.data.source.local.preferences.SettingPreferences
+import com.rizrmdhn.core.data.source.local.preferences.dataStore
+import com.rizrmdhn.core.data.source.local.room.StoryDatabase
+import com.rizrmdhn.core.data.source.remote.RemoteDataSource
+import com.rizrmdhn.core.data.source.remote.network.ApiService
+import com.rizrmdhn.core.domain.repository.IStoryRepository
+import com.rizrmdhn.core.utils.AppExecutors
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val databaseModule = module {
+    factory { get<StoryDatabase>().storyDao() }
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            StoryDatabase::class.java, "Story.db"
+        ).fallbackToDestructiveMigration().build()
+    }
+}
+
+val preferencesModule = module {
+    single {
+        val settingPreferences = SettingPreferences(androidContext().dataStore)
+        settingPreferences
+    }
+}
+
+val networkModule = module {
+    single {
+        val certificatePinner = CertificatePinner.Builder()
+            .add(
+                BuildConfig.BASE_URL_HOSTNAME,
+                "sha256/qRXOtBLL57LL0c7e8w/vou6FL8GMrasDduMdcQqXeBw= "
+            )
+            .add(
+                BuildConfig.BASE_URL_HOSTNAME,
+                "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0="
+            )
+            .build()
+        OkHttpClient.Builder()
+            .addInterceptor(
+                if (BuildConfig.DEBUG) HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+                else HttpLoggingInterceptor().setLevel(
+                    HttpLoggingInterceptor.Level.NONE
+                )
+            )
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
+    }
+    single {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get<OkHttpClient>())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
+
+val repositoryModule = module {
+    single { LocalDataSource(get(), get()) }
+    single { RemoteDataSource(get()) }
+    factory { AppExecutors() }
+    single<IStoryRepository> {
+        StoryRepository(
+            get(),
+            get(),
+            get()
+        )
+    }
 }
